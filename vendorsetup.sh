c02fd4d5c93702deca5e4f67e17d1845e1533de0
UmaruSableng7500 <umarusableng@proton.me>
2026-05-17 21:53:31 -0700

    sweet: impelent vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,20 @@
+# Commons
+git clone https://github.com/sweet-stuffs/device_xiaomi_sm6150-common.git device/xiaomi/sm6150-common
+git clone https://github.com/sweet-stuffs/vendor_xiaomi_sm6150-common.git vendor/xiaomi/sm6150-common
+
+# Device (sweet)
+git clone https://github.com/sweet-stuffs/vendor_xiaomi_sweet.git vendor/xiaomi/sweet
+
+# MIUI Camera
+git clone https://github.com/sweet-stuffs/device_xiaomi_miuicamera-sweet.git device/xiaomi/miuicamera-sweet
+git clone https://github.com/sweet-stuffs/vendor_xiaomi_miuicamera-sweet.git vendor/xiaomi/miuicamera-sweet
+
+# Hardware Xiaomi
+git clone https://github.com/sweet-stuffs/hardware_xiaomi.git hardware/xiaomi
+
+# Kernel SM6150
+git clone https://github.com/PixelOS-Devices/android_kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
+
+# Dolby
+git clone https://github.com/sweet-stuffs/proprietary_vendor_sony_dolby.git vendor/sony/dolby
+git clone https://github.com/sweet-stuffs/android_packages_apps_LunarisDolby.git packages/apps/LunarisDolby
